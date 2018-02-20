e674cebe8a41fb5fca859dc9d262323d9289ba09
WCLOUD\mrondon <mrondon@wcloudservices.com>
2018-02-20 17:35:50 -0400

    seccion 4 clase 30

---
+++ adminpro/src/app/pages/pages.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ]
+})
+
+export class PagesModule { }
