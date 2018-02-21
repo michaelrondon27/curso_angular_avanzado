165510da845ce5afb1e2afc22ac413e6c81d143a
WCLOUD\mrondon <mrondon@wcloudservices.com>
2018-02-21 14:07:07 -0400

    seccion 6 clase 47

--- adminpro/src/app/pages/pages.module.ts
+++ adminpro/src/app/pages/pages.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
+
 import { PAGES_ROUTES } from './pages.routes';
 
 import { SharedModule } from '../shared/shared.module';
+
+import { FormsModule } from '@angular/forms';
 
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 
+// Temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+
 @NgModule({
     declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent
     ],
     exports: [
         DashboardComponent,
@@ -22,7 +29,8 @@ import { Graficas1Component } from './graficas1/graficas1.component';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule
     ]
 })
 
