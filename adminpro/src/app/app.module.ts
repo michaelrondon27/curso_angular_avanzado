88afb16b7afecf059787df21577a3b7b00e979e5
WCLOUD\mrondon <mrondon@wcloudservices.com>
2018-02-21 10:01:24 -0400

    seccion 4 completada

--- adminpro/src/app/app.module.ts
+++ adminpro/src/app/app.module.ts
@@ -7,23 +7,15 @@ import { APP_ROUTES } from './app.routes';
 // Modulos
 import { PagesModule } from './pages/pages.module';
 
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { RegisterComponent } from './login/register.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
     RegisterComponent
   ],
   imports: [
--- adminpro/src/app/pages/pages.module.ts
+++ adminpro/src/app/pages/pages.module.ts
@@ -1,4 +1,8 @@
 import { NgModule } from '@angular/core';
+import { PAGES_ROUTES } from './pages.routes';
+
+import { SharedModule } from '../shared/shared.module';
+
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -15,6 +19,10 @@ import { Graficas1Component } from './graficas1/graficas1.component';
         DashboardComponent,
         ProgressComponent,
         Graficas1Component
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
     ]
 })
 
