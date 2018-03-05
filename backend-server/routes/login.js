42ef55ab42f77c70c8c70e6df44037c6dd96d0a6
WCLOUD\mrondon <mrondon@wcloudservices.com>
2018-03-05 16:02:40 -0400

    seccion 12 completada

--- backend-server/routes/login.js
+++ backend-server/routes/login.js
@@ -8,6 +8,107 @@ var app = express();
 
 var Usuario = require('../models/usuario');
 
+const GOOGLE_CLIENT_ID = require('../config/config').GOOGLE_CLIENT_ID;
+const GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET;
+
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(GOOGLE_CLIENT_ID);
+
+//========================
+// Autenticación de Google
+//========================
+app.post('/google', (req, res, next) => {
+
+    var token = req.body.token;
+
+    const oAuth2Client = new OAuth2Client(
+        GOOGLE_CLIENT_ID,
+        GOOGLE_SECRET
+    );
+
+    const ticket = oAuth2Client.verifyIdToken({
+        idToken: token
+            //audience: GOOGLE_CLIENT_ID
+    });
+
+    ticket.then(data => {
+
+        Usuario.findOne({ email: data.payload.email }, (err, usuario) => {
+
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al buscar usuario',
+                    errors: err
+                });
+            }
+
+            if (usuario) {
+
+                if (!usuario.google) {
+                    return res.status(400).json({
+                        ok: false,
+                        mensaje: 'Debe de usar su atenticación normal'
+                    });
+                } else {
+
+                    // Crear token
+                    usuario.password = ':)';
+                    var token = jwt.sign({ usuario: usuario }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                    res.status(200).json({
+                        ok: true,
+                        usuario: usuario,
+                        id: usuario._id,
+                        token: token
+                    });
+
+                }
+
+            } else {
+
+                var usuario = new Usuario();
+
+                usuario.nombre = data.payload.name;
+                usuario.email = data.payload.email;
+                usuario.password = ':)';
+                usuario.img = data.payload.picture;
+                usuario.google = true;
+
+                usuario.save((err, usuarioDB) => {
+
+                    if (err) {
+                        return res.status(500).json({
+                            ok: false,
+                            mensaje: 'Error al crear usuario',
+                            errors: err
+                        });
+                    }
+
+                    // Crear token
+                    var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                    res.status(200).json({
+                        ok: true,
+                        usuario: usuarioDB,
+                        id: usuarioDB._id,
+                        token: token
+                    });
+
+                });
+
+            }
+
+        });
+
+    });
+
+});
+
+//=====================
+// Autenticación normal
+//=====================
+
 app.post('/', (req, res) => {
 
     var body = req.body;
