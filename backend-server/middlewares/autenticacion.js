95e1a6555ec640eaa97a8a9e260d79b36562d613
WCLOUD\mrondon <mrondon@wcloudservices.com>
2018-04-02 11:41:53 -0400

    validar si es el mismo usuario o un admin

--- backend-server/middlewares/autenticacion.js
+++ backend-server/middlewares/autenticacion.js
@@ -50,3 +50,28 @@ exports.verificaADMIN_ROLE = function(req, res, next) {
     }
 
 }
+
+// ===============================
+// Verificar ADMIN o Mismo Usuario
+// ===============================
+exports.verificaADMIN_o_MismoUsuario = function(req, res, next) {
+
+    var usuario = req.usuario;
+    var id = req.params.id;
+
+    if (usuario.role === 'ADMIN_ROLE' || usuario._id === id) {
+
+        next();
+        return;
+
+    } else {
+
+        return res.status(401).json({
+            ok: false,
+            mensaje: 'Token incorrecto - No es administrador ni es el mismo usuario',
+            errors: { message: 'No es administrador, no puede hacer eso' }
+        });
+
+    }
+
+}
--- backend-server/routes/usuario.js
+++ backend-server/routes/usuario.js
@@ -46,7 +46,7 @@ app.get('/', (req, res, next) => {
 // ==================
 // Actualizar usuario
 // ==================
-app.put('/:id', [mdAutenticacion.verificaToken, mdAutenticacion.verificaADMIN_ROLE], (req, res) => {
+app.put('/:id', [mdAutenticacion.verificaToken, mdAutenticacion.verificaADMIN_o_MismoUsuario], (req, res) => {
 
     var id = req.params.id;
     var body = req.body;
