9256991f641fb4803e06164a89cfbe8b0a22d829
WCLOUD\mrondon <mrondon@wcloudservices.com>
2018-04-03 10:04:39 -0400

    funcion renovar token frontend

--- adminpro/src/app/services/usuario/usuario.service.ts
+++ adminpro/src/app/services/usuario/usuario.service.ts
@@ -25,6 +25,26 @@ export class UsuarioService {
     this.cargarStorage();
   }
 
+  renuevaToken() {
+
+    let url = URL_SERVICIOS + '/login/renuevatoken?token=' + this.token;
+
+    return this.http.get( url ).map( (resp: any) => {
+
+      this.token = resp.token;
+      localStorage.setItem('token', this.token);
+      return true;
+
+    }).catch( err => {
+
+      this.router.navigate(['/login']);
+      swal('No se pudo renovar token', 'No fue posible renovar token', 'error');
+      return Observable.throw( err );
+
+    });
+
+  }
+
   estaLogueado() {
     return ( this.token.length > 5 ) ? true : false;
   }
