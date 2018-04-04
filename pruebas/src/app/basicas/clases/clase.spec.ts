7068b41f43549a6ca103fdde78dafb0cd092939e
WCLOUD\mrondon <mrondon@wcloudservices.com>
2018-04-04 17:12:01 -0400

    probando clases parte 2

--- pruebas/src/app/basicas/clases/clase.spec.ts
+++ pruebas/src/app/basicas/clases/clase.spec.ts
@@ -1,7 +1,11 @@
 import { Jugador } from './clase';
 describe( 'Pruebas de clases', () => {
 
-    const jugador = new Jugador();
+    let jugador: Jugador;
+
+    beforeEach( () => {
+        jugador = new Jugador();
+    });
 
     it( 'Debe de retornar 80 de hp, si recibe 20 de daño', () => {
 
@@ -14,6 +18,13 @@ describe( 'Pruebas de clases', () => {
 
         const resp = jugador.recibeDanio(50);
         expect( resp ).toBe(50);
+
+    });
+
+    it( 'Debe de retornar 0 de hp, si recibe 100 de daño o mas', () => {
+
+        const resp = jugador.recibeDanio(100);
+        expect( resp ).toBe(0);
 
     });
 
