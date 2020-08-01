cbc727f5e9e5c1cbf184b933c3dc0ca103fc1001
michael rondon <mrondon72@gmail.com>
2020-08-01 20:05:32 +0200

    36. AuthRoutingModule

--- 03-adminpro/src/app/app-routing.module.ts
+++ 03-adminpro/src/app/app-routing.module.ts
@@ -2,21 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 // Modulos
+import { AuthRoutingModule } from './auth/auth.routing';
 import { PagesRoutingModule } from './pages/pages.routing';
 
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 const routes: Routes = [
-    {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
-        path: 'register',
-        component: RegisterComponent
-    },
     {
         path: '**',
         component: NopagefoundComponent
@@ -25,8 +16,9 @@ const routes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot( routes ),
-        PagesRoutingModule
+        AuthRoutingModule,
+        PagesRoutingModule,
+        RouterModule.forRoot( routes )
     ],
     exports: [
         RouterModule
