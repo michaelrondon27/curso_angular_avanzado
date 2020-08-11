6d9f5b0758f60e9d23f1a090739620ac78948052
michael rondon <mrondon72@gmail.com>
2020-08-11 13:47:53 +0200

    50. @Output - componente incrementador

--- 03-adminpro/src/app/components/incrementador/incrementador.component.ts
+++ 03-adminpro/src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -8,19 +8,24 @@ import { Component, Input } from '@angular/core';
 })
 export class IncrementadorComponent {
 
-  @Input() progreso: number = 50;
+  @Input('valor') progreso: number = 40;
+
+  @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
 
   cambiarValor( valor: number ) {
 
     if ( this.progreso >= 100 && valor >= 0) {
+      this.valorSalida.emit(100);
       return this.progreso = 100;
     }
 
     if ( this.progreso <= 0 && valor < 0) {
+      this.valorSalida.emit(0);
       return this.progreso = 0;
     }
 
     this.progreso = this.progreso + valor;
+    this.valorSalida.emit(this.progreso);
 
   }
 
