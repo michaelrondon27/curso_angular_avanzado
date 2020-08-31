7a1d7ee6cba9fb44003ed99e090712855ba74bbb
michael rondon <mrondon72@gmail.com>
2020-08-31 15:02:44 +0200

    82. LLamar el unsunscribe

--- 03-adminpro/src/app/pages/rxjs/rxjs.component.ts
+++ 03-adminpro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Observable, interval } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
 import { retry, take, map, filter } from 'rxjs/operators';
 
 @Component({
@@ -8,7 +8,9 @@ import { retry, take, map, filter } from 'rxjs/operators';
   styles: [
   ]
 })
-export class RxjsComponent {
+export class RxjsComponent implements OnDestroy {
+
+  public intervalSubs: Subscription;
 
   constructor() {
 
@@ -20,13 +22,19 @@ export class RxjsComponent {
     //   () => console.info('Obs terminado')
     // );
 
-    this.retornaInterval().subscribe(console.log);
+    this.intervalSubs = this.retornaInterval().subscribe(console.log);
+
+  }
+
+  ngOnDestroy(): void {
+
+    this.intervalSubs.unsubscribe();
 
   }
 
   retornaInterval(): Observable<number> {
 
-    return interval(500).pipe(
+    return interval(100).pipe(
       take(10),
       map( valor => valor + 1),
       filter( valor => ( valor % 2 === 0 ) ? true : false ),
