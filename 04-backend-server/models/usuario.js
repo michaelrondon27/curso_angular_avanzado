c1479a000836129957c50c66be05a453e02f581c
michael rondon <mrondon72@gmail.com>
2020-10-20 20:17:58 +0200

    107. Terminar el GET de los usuarios

--- 04-backend-server/models/usuario.js
+++ 04-backend-server/models/usuario.js
@@ -28,4 +28,12 @@ const UsuarioSchema = Schema({
     }
 });
 
+UsuarioSchema.method('toJSON', function() {
+    const { __v, _id, ...object } = this.toObject();
+
+    object.uid = _id;
+
+    return object;
+});
+
 module.exports = model('Usuario', UsuarioSchema);
