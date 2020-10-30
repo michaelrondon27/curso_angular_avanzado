66690309049a524637d55eec33c15925cc5cb5ba
michael rondon <mrondon72@gmail.com>
2020-10-30 02:22:33 +0100

    117. Revision del token - Middleware

--- 04-backend-server/routes/usuarios.js
+++ 04-backend-server/routes/usuarios.js
@@ -3,13 +3,15 @@
  */
 
 const { Router } = require('express');
-const { actualizarUsuario, borrarUsuario, crearUsuario, getUsuarios } = require('../controllers/usuarios');
 const { check } = require('express-validator');
+
+const { actualizarUsuario, borrarUsuario, crearUsuario, getUsuarios } = require('../controllers/usuarios');
 const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
-router.get('/', getUsuarios);
+router.get('/', validarJWT, getUsuarios);
 
 router.post(
     '/',
@@ -25,9 +27,10 @@ router.post(
 router.put(
     '/:id',
     [
+        validarJWT,
         check('nombre', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El email es obligatorio').isEmail(),
-        check('role', 'El role es obligatorio').isEmail(),
+        check('role', 'El role es obligatorio').not().isEmpty(),
         validarCampos
     ],
     actualizarUsuario
@@ -35,6 +38,7 @@ router.put(
 
 router.delete(
     '/:id',
+    validarJWT,
     borrarUsuario
 );
 
