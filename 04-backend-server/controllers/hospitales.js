41dc5e0ae6451404af7ef8f4839a0af0a15cc9c5
michael rondon <mrondon72@gmail.com>
2020-10-31 01:44:53 +0100

    124. Crear Hospitales

--- 04-backend-server/controllers/hospitales.js
+++ 04-backend-server/controllers/hospitales.js
@@ -1,5 +1,7 @@
 const { response } = require('express');
 
+const Hospital = require('../models/hospital');
+
 const actualizarHospital = (req, res = response) => {
     res.json({
         ok: true,
@@ -14,11 +16,26 @@ const borrarHospital = (req, res = response) => {
     });
 }
 
-const crearHospital = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'crearHospital'
+const crearHospital = async (req, res = response) => {
+    const uid = req.uid;
+    const hospital = new Hospital({
+        usuario: uid,
+        ...req.body
     });
+
+    try {
+        const hospitalDB = await hospital.save();
+
+        res.json({
+            ok: true,
+            hospital: hospitalDB
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
 }
 
 const getHospitales = (req, res = response) => {
--- 04-backend-server/routes/hospitales.js
+++ 04-backend-server/routes/hospitales.js
@@ -16,7 +16,11 @@ router.get('/', getHospitales);
 
 router.post(
     '/',
-    [],
+    [
+        validarJWT,
+        check('nombre', 'El nombre del hospital es necesario').not().isEmpty(),
+        validarCampos
+    ],
     crearHospital
 );
 
