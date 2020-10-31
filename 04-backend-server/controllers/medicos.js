d32398635b067bee8e290b6b2444c3e027ce02d3
michael rondon <mrondon72@gmail.com>
2020-10-31 01:55:38 +0100

    125. Crear medicos

--- 04-backend-server/controllers/medicos.js
+++ 04-backend-server/controllers/medicos.js
@@ -1,5 +1,7 @@
 const { response } = require('express');
 
+const Medico = require('../models/medico');
+
 const actualizarMedico = (req, res = response) => {
     res.json({
         ok: true,
@@ -14,11 +16,26 @@ const borrarMedico = (req, res = response) => {
     });
 }
 
-const crearMedico = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'crearMedico'
+const crearMedico = async (req, res = response) => {
+    const uid = req.uid;
+    const medico = new Medico({
+        usuario: uid,
+        ...req.body
     });
+
+    try {
+        const medicoDB = await medico.save();
+
+        res.json({
+            ok: true,
+            medico: medicoDB
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'hable con el administrador'
+        });
+    }
 }
 
 const getMedicos = (req, res = response) => {
--- 04-backend-server/routes/medicos.js
+++ 04-backend-server/routes/medicos.js
@@ -16,7 +16,11 @@ router.get('/', getMedicos);
 
 router.post(
     '/',
-    [],
+    [
+        validarJWT,
+        check('nombre', 'El nombre del médico es necesario').not().isEmpty(),
+        validarCampos
+    ],
     crearMedico
 );
 
