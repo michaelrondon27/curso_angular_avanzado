524980c1fecd177b1307d4911c54c5c0c48d6a2c
michael rondon <mrondon72@gmail.com>
2020-11-01 03:16:21 +0100

    133. Subir archivos al servidor - Parte 2

--- 04-backend-server/controllers/uploads.js
+++ 04-backend-server/controllers/uploads.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const { v4: uuidv4 } = require('uuid');
 
 const fileUpload = (req, res = response) => {
     const tipo = req.params.tipo;
@@ -7,7 +8,7 @@ const fileUpload = (req, res = response) => {
     // Validar tipo
     const tiposValidos = ['hospitales', 'medicos', 'usuarios'];
 
-    if ( tiposValidos.includes(tipo) ) {
+    if ( !tiposValidos.includes(tipo) ) {
         return res.status(400).json({
             ok: false,
             msg: 'No es médico, usuario u hospital (tipo)'
@@ -23,10 +24,40 @@ const fileUpload = (req, res = response) => {
     }
 
     // Procesar la imagen...
+    const file = req.files.imagen;
+    const nombreCortado = file.name.split('.');
+    const extensionArchivo = nombreCortado[ nombreCortado.length - 1 ];
 
-    res.json({
-        ok: true,
-        msg: 'File uploaded'
+    // Validar extension
+    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
+
+    if ( !extensionesValidas.includes(extensionArchivo) ) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'No es una extensión permitida'
+        });
+    }
+
+    // Generar el nombre del archivo
+    const nombreArchivo = `${ uuidv4() }.${ extensionArchivo }`;
+
+    // Path para guardar la imagen
+    const path = `./uploads/${ tipo }/${ nombreArchivo }`;
+
+    // Mover la imagen
+    file.mv(path, (err) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                msg: 'Error al mover la imagen'
+            });
+        }
+
+        res.json({
+            ok: true,
+            msg: 'Archivo subido',
+            nombreArchivo
+        });
     });
 }
 
