9bd06f0e5d7718c14e82ef791ef289ff83ccf49d
michael rondon <mrondon72@gmail.com>
2020-11-01 02:22:45 +0100

    131. Busqueda por coleccion especifica

--- 04-backend-server/controllers/busquedas.js
+++ 04-backend-server/controllers/busquedas.js
@@ -4,6 +4,42 @@ const Hospital = require('../models/hospital');
 const Medico = require('../models/medico');
 const Usuario = require('../models/usuario');
 
+const getDocumentosColeccion = async (req, res = response) => {
+    const busqueda = req.params.busqueda;
+    const tabla = req.params.tabla;
+    const regex = new RegExp( busqueda, 'i' );
+
+    let data = [];
+
+    switch ( tabla ) {
+        case 'hospitales':
+            data = await Hospital.find({ nombre: regex })
+                            .populate('usuario', 'nombre img');
+            break;
+
+        case 'medicos':
+            data = await Medico.find({ nombre: regex })
+                            .populate('usuario', 'nombre img')
+                            .populate('hospital', 'nombre img');
+            break;
+    
+        case 'usuarios':
+            data = await Usuario.find({ nombre: regex });            
+            break;
+
+        default:
+            return res.status(400).json({
+                ok: false,
+                msg: 'La tabla tiene que ser usuarios/medicos/hospitales'
+            });
+    }
+
+    res.json({
+        ok: true,
+        resultados: data
+    });
+}
+
 const getTodo = async (req, res = response) => {
     const busqueda = req.params.busqueda;
     const regex = new RegExp( busqueda, 'i' );
@@ -23,5 +59,6 @@ const getTodo = async (req, res = response) => {
 }
 
 module.exports = {
+    getDocumentosColeccion,
     getTodo
 }
--- 04-backend-server/routes/busquedas.js
+++ 04-backend-server/routes/busquedas.js
@@ -4,11 +4,12 @@
 
 const { Router } = require('express');
 
-const { getTodo } = require('../controllers/busquedas');
+const { getDocumentosColeccion, getTodo } = require('../controllers/busquedas');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
 router.get('/:busqueda', validarJWT, getTodo);
+router.get('/coleccion/:tabla/:busqueda', validarJWT, getDocumentosColeccion);
 
 module.exports = router;
