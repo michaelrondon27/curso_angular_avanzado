9218899007f52c79a744c432955e0f3d11a6b3bd
michael rondon <mrondon72@gmail.com>
2020-11-06 18:26:46 +0100

    145. Validar token de google en nuestro backend

--- 04-backend-server/controllers/auth.js
+++ 04-backend-server/controllers/auth.js
@@ -1,8 +1,31 @@
-const { response } = require('express');
 const bcrypt = require('bcryptjs');
+const { response } = require('express');
+const {OAuth2Client} = require('google-auth-library');
 
+const { googleVerify } = require('../helpers/google-verify');
 const { generarJWT } = require('../helpers/jwt');
 const Usuario = require('../models/usuario');
+
+const googleSingIn = async (req, res = response) => {
+    const googleToken = req.body.token;
+
+    const { name, email, picture } = await googleVerify( googleToken );
+
+    try {
+        res.json({
+            ok: true,
+            msg: 'Google Sing In',
+            name,
+            email,
+            picture
+        });
+    } catch (error) {
+        res.status(401).json({
+            ok: false,
+            msg: 'Token no es correcto'
+        });
+    }
+}
 
 const login = async (req, res = response) => {
     const { email, password } = req.body;
@@ -44,5 +67,6 @@ const login = async (req, res = response) => {
 }
 
 module.exports = {
+    googleSingIn,
     login
 }
--- 04-backend-server/routes/auth.js
+++ 04-backend-server/routes/auth.js
@@ -4,19 +4,28 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { googleSingIn, login } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.post(
-    '',
+    '/',
     [
         check('email', 'El email es obligatorio').isEmail(),
         check('password', 'El password es obligatorio').not().isEmpty(),
         validarCampos
     ],
     login
+);
+
+router.post(
+    '/google',
+    [
+        check('token', 'El token de Google es obligatorio').not().isEmpty(),
+        validarCampos
+    ],
+    googleSingIn
 );
 
 module.exports = router;
