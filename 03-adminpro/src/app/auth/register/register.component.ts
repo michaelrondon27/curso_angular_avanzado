65dbb34857799955dd78efded3e7e3fb23f9786e
michael rondon <mrondon72@gmail.com>
2020-11-15 20:45:02 +0100

    163. Validaciones del formulario

--- 03-adminpro/src/app/auth/register/register.component.ts
+++ 03-adminpro/src/app/auth/register/register.component.ts
@@ -10,11 +10,13 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
 
+  public formSubmitted = false;
+
   public registerForm = this.fb.group({
     nombre: ['Michael', Validators.required],
-    email: ['test100@gmail.com', Validators.required],
-    password: ['123456', Validators.required],
-    password2: ['123456', Validators.required],
+    email: ['test100@gmail.com', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    password2: ['', Validators.required],
     terminos: [false, Validators.required]
   });
 
@@ -22,8 +24,27 @@ export class RegisterComponent {
     private fb: FormBuilder
   ) { }
 
+  aceptaTerminos() {
+    return !this.registerForm.get('terminos').value && this.formSubmitted;
+  }
+
+  campoNoValido( campo: string ): boolean {
+    if ( this.registerForm.get(campo).invalid && this.formSubmitted ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   crearUsuario() {
+    this.formSubmitted = true;
     console.log(this.registerForm.value);
+
+    if ( this.registerForm.valid ) {
+      console.log('Posteando formulario');
+    } else {
+      console.log('Formulario no es correcto');
+    }
   }
 
 }
